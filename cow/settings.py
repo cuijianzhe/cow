e50da6b6e538c43795fe73f9cbb78a72e0fe10c4
cuijianzhe <598941324@qq.com>
2021-02-20 20:22:15 +0800

    添加基础models类

--- cow/settings.py
+++ cow/settings.py
@@ -40,13 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'cow.urls'
